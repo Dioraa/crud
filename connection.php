e7097e8ff152b47eaa08f0d288213d3175784c54
Dioraa <160730627+Dioraa@users.noreply.github.com>
2024-11-03 11:13:59 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,13 +1,13 @@
 <?php
 	//for MySQLi OOP
-	$conn = new mysqli('127.0.0.1:3307', 'root', '', 'roikursi');
+	$conn = new mysqli('', 'root', '', '');
 	if($conn->connect_error){
 	   die("Connection failed: " . $conn->connect_error);
 	}
 	////////////////
 
 	//for MySQLi Procedural
-	// $conn = mysqli_connect('localhost', 'root', '', 'roikursi');
+	// $conn = mysqli_connect('localhost', 'root', '', '');
 	// if(!$conn){
 	//     die("Connection failed: " . mysqli_connect_error());
 	// }
